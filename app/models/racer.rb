63327432c62873392d29f9122728662da21cd1e1
Alberto Penas Amor <alberto__pa@hotmail.com>
2016-04-28 19:14:10 +0200

    added relationship between racer and racerinfo

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,3 +1,8 @@
 class Racer
   include Mongoid::Document
+  embeds_one :info, class_name: 'RacerInfo', as: :parent, autobuild: true
+
+  before_create do |racer|
+    racer.info.id = racer.id
+  end
 end
