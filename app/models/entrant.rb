184386c8f634444d4aabb78f33d34cde4ee61fb1
Alberto Penas Amor <alberto__pa@hotmail.com>
2016-05-01 01:49:21 +0200

    added flattened properties from entrant to result

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -25,10 +25,44 @@ class Entrant
   embeds_one :race, class_name: 'RaceRef', autobuild: true
   embeds_one :racer, class_name: 'RacerInfo', as: :parent, autobuild: true
 
+  RESULTS = {"swim"=>SwimResult,
+             "t1"=>LegResult,
+             "bike"=>BikeResult,
+             "t2"=>LegResult,
+             "run"=>RunResult}
+
+  RESULTS.keys.each do |name|
+    #create_or_find result
+    define_method("#{name}") do
+      result=results.select {|result| name==result.event.name if result.event}.first
+      if !result
+        result=RESULTS["#{name}"].new(:event=>{:name=>name})
+        results << result
+      end
+      result
+    end
+    #assign event details to result
+    define_method("#{name}=") do |event|
+      event=self.send("#{name}").build_event(event.attributes)
+    end
+    #expose setter/getter for each property of each result
+    RESULTS["#{name}"].attribute_names.reject {|r|/^_/===r}.each do |prop|
+      define_method("#{name}_#{prop}") do
+        event=self.send(name).send(prop)
+      end
+      define_method("#{name}_#{prop}=") do |value|
+        event=self.send(name).send("#{prop}=",value)
+        update_total nil if /secs/===prop
+      end
+    end
+  end
+
   def update_total(result)
     sum = 0
     results.each do |res|
-      sum += res.secs
+      if !res.nil? && !res.secs.nil?
+        sum += res.secs
+      end
     end
     self.secs = sum
   end
