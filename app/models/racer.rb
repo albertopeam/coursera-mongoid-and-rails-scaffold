684cca43b8976c63dd76dbaf96e2dcb9448c406a
Alberto Penas Amor <alberto__pa@hotmail.com>
2016-04-30 17:06:06 +0200

    relationship between racer and entrant

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,7 +1,11 @@
 class Racer
   include Mongoid::Document
-  
+
   embeds_one :info, class_name: 'RacerInfo', as: :parent, autobuild: true
+  has_many :races, class_name: 'Entrant',
+                   foreign_key: "racer.racer_id",
+                   dependent: :nullify,
+                   order: :"race.date".desc
 
   before_create do |racer|
     racer.info.id = racer.id
